a782a750c9d3a287108472a1a05d588c9b1ee3be
Sheikh Shafi <shafiss01318020612@gmail.com>
2022-05-04 20:20:56 +0600

    update button complete

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const app = express();
 var cors = require("cors");
 const port = process.env.PORT || 5000;
-
 
 // username = bigbazar
 // password = oizxMIb2xPevh2cP
@@ -21,20 +20,54 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-      const productCollection = client.db("bigbazar").collection("product");
-      app.post("/post", async(req, res) => {
-          const product = req.body;
+    const productCollection = client.db("bigbazar").collection("product");
+    app.post("/post", async (req, res) => {
+      const product = req.body;
       console.log(product);
-          const result = await productCollection.insertOne(product);
-          res.send(result)
-      })
-      app.get("/productslist", async (req, res) => {
-          const cursor = productCollection.find({});
-          const result = await cursor.toArray();
-          res.send(result);
+      const result = await productCollection.insertOne(product);
+      res.send({ success: "success fully post" });
+    });
+    app.get("/productslist", async (req, res) => {
+      const cursor = productCollection.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // --------for homo page only 6 cards --------
+    app.get("/productslisthome", async (req, res) => {
+      const cursor = productCollection.find({});
+      const result = await cursor.limit(6).toArray();
+      res.send(result);
+    });
 
-      })
-      
+//---------- delete product----------    
+    app.delete("/delete/:productId", async(req, res) => {
+      const productId = req.params.productId;
+      const query = { _id: ObjectId(productId) };
+      const result = await productCollection.deleteOne(query)
+      res.send(result);
+    });
+
+    //-------update Product ----------
+    app.put('/update/:id', async (req, res) => {
+      const id = req.params.id;
+      const updateProduct = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: updateProduct.name,
+          img: updateProduct.img,
+          price: updateProduct.price,
+          quantity: updateProduct.quantity,
+          info: updateProduct.info,
+          supliarName: updateProduct.supliarName
+        },
+      };
+      const result = await productCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    })
+
+
   } finally {
     // await client.close();
   }
