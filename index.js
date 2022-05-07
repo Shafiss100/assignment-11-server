c697954e5104cea921eab40c29c6437a4110ae15
Sheikh Shafi <shafiss01318020612@gmail.com>
2022-05-07 21:14:33 +0600

    received quantity and send complete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
+var jwt = require("jsonwebtoken");
 const app = express();
 var cors = require("cors");
 const port = process.env.PORT || 5000;
@@ -10,6 +11,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 require("dotenv").config();
+
+
+// const verifyJwt = (req, res, next) => {
+//   const authHeader = req.headers.authorization;
+//   if (!authHeader) {
+//     return res.status(401).send({ message: 'unauthorize access' });
+//   }
+//   const token = authHeader.split(' ')[1];
+//   jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, function (err, decoded) {
+//     if (err) {
+//       return res.status(403).send({message:'forbidden access'})
+//     }
+//       req.decoded = decoded
+//   });
+//   next();
+// }
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.0x0jt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -32,7 +49,16 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
-    // --------for homo page only 6 cards --------
+// ----------- find one by id----------
+    app.get("/productslist/findone", async (req, res) => {
+      const id = req.query.id;
+      console.log(id);
+      const filter = { _id: ObjectId(id) };
+      const result = await orderCollection.findOne(filter).toArray();
+      console.log(result);
+      res.send(result);
+    });
+// --------for homo page only 6 cards --------
     app.get("/productslisthome", async (req, res) => {
       const cursor = productCollection.find({});
       const result = await cursor.limit(6).toArray();
@@ -48,7 +74,7 @@ async function run() {
     });
 
     //-------update Product ----------
-    app.put('/update/:id', async (req, res) => {
+    app.put('/productupdate/:id', async (req, res) => {
       const id = req.params.id;
       const updateProduct = req.body;
       const filter = { _id: ObjectId(id) };
@@ -60,12 +86,47 @@ async function run() {
           price: updateProduct.price,
           quantity: updateProduct.quantity,
           info: updateProduct.info,
-          supliarName: updateProduct.supliarName
+          supliarName: updateProduct.supliarName,
         },
       };
-      const result = await productCollection.updateOne(filter, updateDoc, options);
+      const result = await productCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
       res.send(result);
+    });
+    //-------update order ----------
+    app.put('/orderupdate/:id', async (req, res) => {
+      const id = req.params.id;
+      const updateProduct = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: {
+          name: updateProduct.name,
+          img: updateProduct.img,
+          price: updateProduct.price,
+          quantity: updateProduct.quantity,
+          info: updateProduct.info,
+          supliarName: updateProduct.supliarName,
+        },
+      };
+      const result = await orderCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+ // ------------ token---------
+      app.post("/token", async (req, res) => {
+        const user = req.body.email;
+        const accessToken = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN);
+        res.send({ accessToken });
+      });
     })
+
+
 // ----------- add order-----------
     app.post("/order", async (req, res) => {
          const order= req.body;
@@ -74,13 +135,21 @@ async function run() {
 
     })
 
+
+
 // ------------ order list ---------
     app.get("/orderlist", async (req, res) => {
+      const decodedEmail = req.decoded;
       const email = req.query.email;
-      const query = {email: email}
+      // if (email === decodedEmail) {
+      const query = { email: email };
       const cursor = orderCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
+    // }
+      // else {
+      //   res.status(403).send({ message: 'forbidden access' });
+      // }
     })
 // ------delete from order list-------
     app.delete("/orderdelete", async (req, res) => {
@@ -88,9 +157,9 @@ async function run() {
       const query = { _id: ObjectId(id) };
       const result = await orderCollection.deleteOne(query);
       if (result.deletedCount === 1) {
-        console.log("Successfully deleted one document.");
+        res.send("Successfully deleted one document.");
       } else {
-        console.log("No documents matched the query. Deleted 0 documents.");
+        res.send("No documents matched the query. Deleted 0 documents.");
       }
     })
   } finally {
